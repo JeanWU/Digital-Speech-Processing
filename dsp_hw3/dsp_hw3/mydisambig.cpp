d770463e325c6cbafcd6475515f97a44fc7bcc1c
JeanWU <r06944023@csie.ntu.edu.tw>
2018-12-08 18:17:47 +0800

    hw3 update

--- dsp_hw3/dsp_hw3/mydisambig.cpp
+++ dsp_hw3/dsp_hw3/mydisambig.cpp
@@ -1,141 +1,183 @@
-#include <cstdio>
-#include <cstdlib>
-#include <iostream>
-#include <fstream>
-#include "Vocab.h"
-#include "VocabMap.h"
 #include "File.h"
-#include "Prob.h"
 #include "Ngram.h"
+#include "Prob.h"       // Probability
+#include "LM.h"         // Language Model
+#include "Vocab.h"      // Vocabular
+#include "VocabMap.h"   // Vocabulary map
 
+/* Reference
+ * http://www.speech.sri.com/projects/srilm/manpages/
+ */
 
+#define FILE_LENGTH 20
 using namespace std;
 
-Vocab ZhuYin, Big5, voc;
-VocabIndex graphidx[200][1000];
-int Back_table[200][1000];
+int find_index(int idx, Vocab voc, Vocab Big5);
+LogP check_backoff(LogP logp, LogP backoff);
+void Viterbi(char *input_text, char *input_map, char *input_lm, int order);
+void find_max_path(int count, int bound, LogP prob[][1000], Vocab Big5, VocabIndex vidxpath[][1000], int backtrack[][1000], VocabString *finalpath);
+void print_path(VocabString path[], int count);
 
-bool is_back(LogP prob, LogP back_prob,LogP look_up, LogP maxp){
-    if ((prob+look_up)>maxp)  return true;
-    return false;
-}
-int find_max_prob_idx(int idx){
-  VocabIndex tmp_word = voc.getIndex(Big5.getWord(idx));
-  if (tmp_word == Vocab_None)
-        tmp_word = voc.getIndex(Vocab_Unknown);
-  return tmp_word;
+
+int main(int argc, char *argv[]){
+  int order = atoi(argv[8]);
+
+  char input_text[FILE_LENGTH],   //1~10.txt
+       input_map[FILE_LENGTH],   //ZhuYin-Big5.map
+       input_lm[FILE_LENGTH];   //bigram.lm
+
+  strcpy(input_text, argv[2]);
+  strcpy(input_map, argv[4]);
+  strcpy(input_lm, argv[6]);
+
+  Viterbi(input_text, input_map, input_lm, order);
+
+  return 0;
 }
 
-int find_max_prob(LogP max_p, int num_cad,LogP prob[]){
-  int idx_col = -87;
-    for(int i=0; i<num_cad;i++){
-        if(prob[i]>max_p){
-          max_p = prob[i];
-          idx_col= i;
-        }
+
+void Viterbi(char *input_text, char *input_map, char *input_lm, int order){
+  Vocab voc, ZhuYin, Big5;
+
+  //read input map and language model
+  VocabMap map(ZhuYin, Big5);
+  File mapfile(input_map, "r");
+  map.read(mapfile);
+  mapfile.close();
+
+  Ngram lm(voc, order);
+  File lmfile(input_lm, "r");
+  lm.read(lmfile);
+  lmfile.close();
+
+  //read input text file
+  File textfile(input_text, "r");
+  char *buffer;
+  //distinguish ambiguity line by line
+  while(buffer=textfile.getline()){
+    //define variables
+    LogP prob[200][1000]={{0.0}};
+    VocabIndex vidxpath[200][1000];
+    int backtrack[200][1000];
+    int candidate_cnt[200];
+
+    Prob p;
+    VocabIndex vidx;
+    VocabIndex empty_context[]={Vocab_None};
+    VocabIndex bigram_context[]={Vocab_None, Vocab_None};
+
+    //parse line to words
+    VocabString sentence[maxWordsPerLine];
+    int count=Vocab::parseWords(buffer, &(sentence[1]), maxWordsPerLine);
+    sentence[0]="<s>";
+    sentence[count+1]="</s>";
+    count+=2;
+
+    //initialize
+    VocabMapIter iter(map, ZhuYin.getIndex(sentence[0]));
+    iter.init();
+    int size=0;
+    while (iter.next(vidx,p)){
+      VocabIndex candidate_idx=find_index(vidx, voc, Big5);
+      LogP logp=lm.wordProb(candidate_idx, empty_context);
+      prob[0][size]=(logp == LogP_Zero)? -66: logp;
+      vidxpath[0][size]=vidx;
+      backtrack[0][size]=-1;
+      ++size;
     }
-    return idx_col;
-}
+    candidate_cnt[0]=size;
 
-void print_ans(VocabString Word[], int count){
-  for (int i = 0; i < count; i++)
-    (i==count-1)? cout<<Word[i]<<endl : cout<<Word[i]<<" ";
-}
+    //recursion
+    for (int i=1; i<count; ++i){
+      VocabMapIter iter(map, ZhuYin.getIndex(sentence[i]));
+      iter.init();
+      size=0;
+      while (iter.next(vidx,p)){
+        VocabIndex candidate_idx=find_index(vidx, voc, Big5);
 
+        LogP maxp=LogP_Zero;
+        for (int j=0; j<candidate_cnt[i-1]; ++j){
+          bigram_context[0]=find_index(vidxpath[i-1][j], voc, Big5);
 
-
-int main (int argc, char* argv[]) {
-    VocabMap map(ZhuYin, Big5);
-
-    File textfile(argv[2], "r");
-    File mapFile(argv[4], "r");
-    map.read(mapFile);
-    mapFile.close();
-
-
-    Ngram lookup(voc, atoi(argv[8]));
-    File lookupFile(argv[6], "r");
-    lookup.read(lookupFile);
-    lookupFile.close();
-
-
-
-
-    char* buffer;
-    while(buffer = textfile.getline()) {
-
-        LogP prob[200][1000] = {{0.0}};
-        int num_cad[200];
-
-        VocabString sentence[maxWordsPerLine];
-
-        Prob p;
-        VocabIndex v_idx;
-        VocabIndex empty_context[] = {Vocab_None};
-        VocabIndex bi_context[] = {Vocab_None, Vocab_None};
-
-        int count = Vocab::parseWords(buffer, &(sentence[1]), maxWordsPerLine);
-        sentence[0] = "<s>";
-        sentence[count+1] = "</s>";
-
-        VocabMapIter iter(map, ZhuYin.getIndex(sentence[0]));
-        iter.init();
-      
-        //  initialize
-        int size = 0;
-        while (iter.next(v_idx, p)) {
-            VocabIndex cad_idx = find_max_prob_idx(v_idx);
-            LogP logp = lookup.wordProb(cad_idx, empty_context);
-            prob[0][size] = (logp == LogP_Zero)  ? -87 : logp;
-            graphidx[0][size] = v_idx;
-            Back_table[0][size] = -87;
-            size++;
+          LogP logp=lm.wordProb(candidate_idx, bigram_context);
+          LogP backoff=lm.wordProb(candidate_idx, empty_context);
+          logp=check_backoff(logp, backoff);
+          logp+=prob[i-i][j];
+          if (logp > maxp){
+            maxp=logp;
+            backtrack[i][size]=j;
+          }
         }
-        num_cad[0] = size;
-
-        // viterbi
-        for (int i = 1; i < count+2; i++) {
-            VocabMapIter iter(map, ZhuYin.getIndex(sentence[i]));
-            iter.init();
-            size = 0;
-            while (iter.next(v_idx, p)) {
-                VocabIndex cad_idx = find_max_prob_idx(v_idx);
-
-                // evaluate max prob
-                LogP maxp = LogP_Zero;
-                for (int j = 0; j < num_cad[i-1]; j++) {
-                    bi_context[0] = find_max_prob_idx(graphidx[i-1][j]);
-                    LogP logp = lookup.wordProb(cad_idx, bi_context);
-                    LogP backoff = lookup.wordProb(cad_idx, empty_context);
-
-                    // check back tracking
-                    if (is_back(logp,backoff,prob[i-1][j],maxp)) {
-                        maxp = logp+prob[i-1][j];
-                        Back_table[i][size] = j;
-                    }
-                    else  logp == -87;
-                    logp += prob[i-1][j];
-                }
-                prob[i][size] = maxp;
-                graphidx[i][size] = v_idx;
-                size++;
-            }
-            num_cad[i] = size;
-        }
-
-        int max_col = find_max_prob(LogP_Zero,num_cad[count+1],prob[count+1]);
-
-        VocabString Word[maxWordLength];
-        Word[0] = "<s>";
-        Word[count+1] = "</s>";
-        for (int i = count+1; i > 0; i--) {
-            Word[i] = Big5.getWord(graphidx[i][max_col]);
-            max_col = Back_table[i][max_col];
-        }
-        print_ans(Word,count+2);
-
+        prob[i][size]=maxp;
+        vidxpath[i][size]=vidx;
+        ++size;
+      }
+      candidate_cnt[i]=size;
     }
-    textfile.close();
 
-    return 0;
+    //find the path with maximum probability
+    int bound=candidate_cnt[count-1];
+    VocabString finalpath[maxWordLength];
+    find_max_path(count, bound, prob, Big5, vidxpath, backtrack, finalpath);
+    print_path(finalpath, count);
+  }
+  textfile.close();
+
 }
+
+
+void print_path(VocabString path[], int count){
+  for(int i=0; i<count; ++i){
+    if(i==count-1){
+      cout<<path[i]<<endl;
+    }
+    else{
+      cout<<path[i]<<" ";
+    }
+  }
+}
+
+
+void find_max_path(int count, int bound, LogP prob[][1000], Vocab Big5, VocabIndex vidxpath[][1000], int backtrack[][1000], VocabString *finalpath){
+  LogP maxp=LogP_Zero;
+  int max_col=-1;
+  for (int j=0; j<bound; ++j){
+    if (prob[count-1][j] > maxp){
+      maxp=prob[count-1][j];
+      max_col=j;
+    }
+  }
+
+  //VocabString finalpath[maxWordLength];
+  
+  finalpath[0]="<s>";
+  //print_path(final_path, count);
+
+  finalpath[count-1]="</s>";
+  for (int i=count-1; i>0; --i){
+    finalpath[i]=Big5.getWord(vidxpath[i][max_col]);
+    //cout<<finalpath[i]<<endl;
+    max_col=backtrack[i][max_col];
+  }
+  
+}
+
+
+LogP check_backoff(LogP logp, LogP backoff){
+  if (logp==backoff && logp==LogP_Zero){
+    logp==-66;
+  }
+  return logp;
+}
+
+
+int find_index(int idx, Vocab voc, Vocab Big5){
+  //check if the word can be found in voc,
+  //if not, set return index == Vocab_Unknown
+  VocabIndex re_idx=voc.getIndex(Big5.getWord(idx));
+  if (re_idx==Vocab_None){
+    re_idx=voc.getIndex(Vocab_Unknown);
+  }
+  return re_idx;
+}
+
